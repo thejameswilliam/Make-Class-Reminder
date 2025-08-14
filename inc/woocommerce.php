1c3f41f73e0080b357a8aaaaf64908fcf97f5b33
James W Johnson <me@thejameswilliam.com>
2025-08-14 12:11:04 -0600

    added tickets sold column.

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -4,7 +4,6 @@
 
 add_action('woocommerce_account_dashboard', 'make_add_upcoming_instructor_classes', 26);
 function make_add_upcoming_instructor_classes() {
-    
     $user_id = get_current_user_id();
     $now = current_time('Y-m-d H:i:s');
     $sub_events = new WP_Query(array(
@@ -34,11 +33,12 @@ function make_add_upcoming_instructor_classes() {
         echo '<div class="upcoming-classes alert alert-light">';
             echo '<h2>You\'re Teaching the Following Classes</h2>';
             echo '<div class="table-responsive">';
-                echo '<table class="table table-striped align-middle">';
+                echo '<table class="table table-striped table-sm align-middle">';
                     echo '<thead><tr>';
                         echo '<th>Class</th>';
                         echo '<th>Date & Time</th>';
                         echo '<th>Add to Calendar</th>';
+                        echo '<th>Tickets Sold</th>';
                     echo '</tr></thead>';
                     echo '<tbody>';
                     while($sub_events->have_posts()) {
@@ -46,11 +46,17 @@ function make_add_upcoming_instructor_classes() {
                         $event_date = get_post_meta(get_the_ID(), 'event_time_stamp', true);
                         $event_date_formatted = date_i18n('l, ' . get_option('date_format') . ' ' . get_option('time_format'), strtotime($event_date));
                         $event_title = get_the_title(get_post_parent(get_the_ID()));
-                        $event_link = get_permalink();
+                        $linked_product = get_post_meta(get_the_ID(), 'linked_product', true);
+                        $woocommerce_product = wc_get_product($linked_product);
+                        $product_stock = $woocommerce_product ? $woocommerce_product->get_stock_quantity() : 0;
+                        $starting_ticket_stock = get_post_meta(get_the_ID(), 'ticket_stock', true);
+                        $event_parent = get_post_parent(get_the_ID());
+
                         echo '<tr>';
-                            echo '<td><a href="' . esc_url($event_link) . '">' . esc_html($event_title) . '</a></td>';
+                            echo '<td><a href="' . esc_url(get_permalink($event_parent)) . '">' . esc_html($event_title) . '</a></td>';
                             echo '<td>' . esc_html($event_date_formatted) . '</td>';
                             echo '<td>' . make_get_event_add_to_calendar_links(get_the_ID()) . '</td>';
+                            echo '<td>Tickets Sold: ' . ($starting_ticket_stock - $product_stock) . '</td>';
                         echo '</tr>';
                     }
                     echo '</tbody>';
