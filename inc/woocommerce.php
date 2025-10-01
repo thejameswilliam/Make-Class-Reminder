8703bf54252d41004c2872f4a4b1eefbf62268d3
James W. Johnson <me@thejameswilliam.com>
2025-10-01 12:43:03 -0600

    instructor section now shows classes that are happening today, despire their start time.

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -5,29 +5,32 @@
 add_action('woocommerce_account_dashboard', 'make_add_upcoming_instructor_classes', 26);
 function make_add_upcoming_instructor_classes() {
     $user_id = get_current_user_id();
-    $now = current_time('Y-m-d H:i:s');
-    $sub_events = new WP_Query(array(
-        'post_type'      => 'sub_event',
-        'posts_per_page' => -1,
-        'orderby'        => 'meta_value',
-        'meta_key'       => 'event_start_time_stamp',
-        'meta_type'      => 'DATETIME',
-        'order'          => 'ASC',
-        'meta_query'     => array(
-            'relation' => 'AND',
-            array(
-                'key'     => 'event_start_time_stamp',
-                'value'   => $now,
-                'compare' => '>=',
-                'type'    => 'DATETIME'
-            ),
-            array(
-                'key' => 'instructorID',
-                'value' => $user_id,
-                'compare' => '='
-            ),
+
+    $defaults = array(
+      'meta_query' => array(
+        array(
+            'key' => 'event_start_time_stamp', // Check the start date field
+            'value' => date('Y-m-d 00:00:00'), // Set today's date (note the similar format)
+            'compare' => '>=', // Return the ones greater than today's date
+            'type' => 'DATETIME' // Let WordPress know we're working with date
         ),
-    ));
+         array(
+            'key' => 'instructorID',
+            'value' => $user_id,
+            'compare' => '='
+        ),
+      ),
+      'orderby' => 'meta_value',
+      'meta_key' => 'event_start_time_stamp',
+      'meta_type' => 'DATETIME',
+      'order' => 'ASC',
+      'post_type' => 'sub_event',
+      'suppress_filters' => true,
+      'posts_per_page' => -1
+    );
+
+
+    $sub_events = new WP_Query($defaults);
 
     if($sub_events->have_posts()) {
         echo '<div class="upcoming-classes alert alert-light">';
@@ -43,7 +46,7 @@ function make_add_upcoming_instructor_classes() {
                     echo '<tbody>';
                     while($sub_events->have_posts()) {
                         $sub_events->the_post();
-                        $event_date = get_post_meta(get_the_ID(), 'event_time_stamp', true);
+                        $event_date = get_post_meta(get_the_ID(), 'event_start_time_stamp', true);
                         $event_date_formatted = date_i18n('l, ' . get_option('date_format') . ' ' . get_option('time_format'), strtotime($event_date));
                         $event_title = get_the_title(get_post_parent(get_the_ID()));
                         $linked_product = get_post_meta(get_the_ID(), 'linked_product', true);
@@ -53,10 +56,10 @@ function make_add_upcoming_instructor_classes() {
                         $event_parent = get_post_parent(get_the_ID());
 
                         echo '<tr>';
-                            echo '<td><a href="' . esc_url(get_permalink($event_parent)) . '">' . esc_html($event_title) . '</a></td>';
-                            echo '<td>' . esc_html($event_date_formatted) . '</td>';
-                            echo '<td>' . make_get_event_add_to_calendar_links(get_the_ID()) . '</td>';
-                            echo '<td>Tickets Sold: ' . ($starting_ticket_stock - $product_stock) . '</td>';
+                            echo '<td class="text-nowrap"><a href="' . esc_url(get_permalink($event_parent)) . '">' . esc_html($event_title) . '</a></td>';
+                            echo '<td class="text-nowrap">' . esc_html($event_date_formatted) . '</td>';
+                            echo '<td class="text-nowrap">' . make_get_event_add_to_calendar_links(get_the_ID()) . '</td>';
+                            echo '<td class="text-nowrap">Tickets Sold: ' . ($starting_ticket_stock - $product_stock) . '</td>';
                         echo '</tr>';
                     }
                     echo '</tbody>';
